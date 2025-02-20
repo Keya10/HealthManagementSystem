14c4c3f1020087a6d531e034c38c2d0707d528c2
codeholic <72653947+Keya10@users.noreply.github.com>
2025-02-20 18:21:50 +0300

    doctors models

--- hms/models.py
+++ hms/models.py
@@ -57,3 +57,32 @@ class Patient(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name} ({self.national_id})"
+    
+    class Doctor(models.Model):
+        national_id = models.CharField(max_length=20, unique=True, validators=[RegexValidator(r'^\d{1,10}$', message='Only digits are allowed')])
+        first_name = models.CharField(max_length=50)
+        last_name = models.CharField(max_length=50)
+        date_of_birth = models.DateField()
+        GENDER_CHOICES = [
+            ('M', 'Male'),
+            ('F', 'Female'),
+        ]
+        gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+        phone_number = models.CharField(max_length=15, validators=[RegexValidator(regex='^\+?1?\d{9,15}$', message='Phone number must be in the format: +254712345678.')])
+        email = models.EmailField(max_length=50)
+        address = models.CharField(max_length=100)
+        specialization = models.CharField(max_length=100)
+        # Professional details
+        license_number = models.CharField(
+            max_length=20,
+            unique=True,
+            validators=[RegexValidator(regex='^[A-Za-z0-9]{5,20}$', message='License number must be alphanumeric and 5-20 characters long.')]
+        )
+        years_of_experience = models.PositiveIntegerField()
+
+        # Timestamps
+        created_at = models.DateTimeField(auto_now_add=True)
+        updated_at = models.DateTimeField(auto_now=True)
+
+        def __str__(self):
+            return f"Dr. {self.first_name} {self.last_name} ({self.specialization})"
