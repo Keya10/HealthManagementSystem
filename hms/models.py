e19842cdc4aa483e4af828f394755faffff86bc1
codeholic <72653947+Keya10@users.noreply.github.com>
2025-02-23 15:11:49 +0300

    created models for department and specalization

--- hms/models.py
+++ hms/models.py
@@ -180,3 +180,35 @@ class Billing(models.Model):
 
     def __str__(self):
         return self.bill_number
+
+#creating depatments in the hospital
+class Department(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+#creating model for nurses and doctors specialization
+class Specialization(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
+#creating model for lab tests
+class LabTest(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+    
