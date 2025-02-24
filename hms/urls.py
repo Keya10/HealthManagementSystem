0e1bdd7f1073b5fe1e520e81c5cf295123f799e4
codeholic <72653947+Keya10@users.noreply.github.com>
2025-02-24 13:58:35 +0300

    updated the homepage

--- hms/urls.py
+++ hms/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path, include
 from . import views
 
@@ -43,3 +45,7 @@ urlpatterns = [
     
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
